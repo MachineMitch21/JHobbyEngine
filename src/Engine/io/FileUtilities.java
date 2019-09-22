a88bf350c56ea18ee89e8930d90f056172f781cb
MachineMitch21 <mitchschutt@gmail.com>
2019-09-22 15:59:07 -0500

    Added FileUtilities class

---
+++ src/Engine/io/FileUtilities.java
@@ -0,0 +1,21 @@
+package Engine.io;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileUtilities {
+    public static String readFile(String file) throws IOException {
+        String data = "";
+
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+
+        String line = "";
+        while ((line = reader.readLine()) != null) {
+            data.concat(line);
+        }
+
+        return data;
+    }
+}
