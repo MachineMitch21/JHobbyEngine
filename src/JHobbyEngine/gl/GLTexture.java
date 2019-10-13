813e1820e8091211b7ae94efb45be7bf97d43c7a
MachineMitch21 <mitchschutt@gmail.com>
2019-10-13 11:12:39 -0500

    Add GLTexture class and readImageFile method to FileUtilities

--- src/JHobbyEngine/gl/GLTexture.java
+++ src/JHobbyEngine/gl/GLTexture.java
@@ -1,6 +1,10 @@
 package JHobbyEngine.gl;
 
 import JHobbyEngine.StringCallback;
+import org.lwjgl.BufferUtils;
+
+import java.awt.image.BufferedImage;
+import java.nio.ByteBuffer;
 
 import static org.lwjgl.opengl.GL33.*;
 
@@ -11,7 +15,38 @@ public class GLTexture {
         this.id = glGenTextures();
     }
 
-    public void create(String fileData, StringCallback cb) {
+    public void create(BufferedImage image, boolean readAlpha, StringCallback cb) {
+        int[] pixels = new int[image.getWidth() * image.getHeight()];
+        pixels = image.getRGB(0, 0, image.getWidth(), image.getHeight(), pixels, 0, image.getWidth());
 
+        final int BYTES_PER_PIXEL = (readAlpha ? 4 : 3);
+
+        ByteBuffer buffer = BufferUtils.createByteBuffer(image.getWidth() * image.getHeight() * BYTES_PER_PIXEL);
+
+        for (int y = 0; y < image.getHeight(); y++) {
+            for (int x = 0; x < image.getWidth(); x++) {
+                int pixel = pixels[y * image.getWidth() + x];
+                buffer.put((byte) ((pixel >> 16) & 0xFF));
+                buffer.put((byte) ((pixel >> 8) & 0xFF));
+                buffer.put((byte) (pixel & 0xFF));
+                if (readAlpha) {
+                    buffer.put((byte) ((pixel >> 24) & 0xFF));
+                }
+            }
+        }
+
+        buffer = buffer.flip();
+
+        glBindTexture(GL_TEXTURE_2D, this.id);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, image.getWidth(), image.getHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE, buffer);
+        glBindTexture(GL_TEXTURE_2D, 0);
+    }
+
+    public void destroy() {
+        glDeleteTextures(this.id);
     }
 }
--- src/JHobbyEngine/io/FileUtilities.java
+++ src/JHobbyEngine/io/FileUtilities.java
@@ -1,10 +1,19 @@
 package JHobbyEngine.io;
 
+import org.lwjgl.BufferUtils;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class FileUtilities {
+
     public static String readFile(String file, boolean keepNewLines) throws IOException {
         String data = "";
 
@@ -20,5 +29,9 @@ public class FileUtilities {
 
     public static String readFile(String file) throws IOException {
         return readFile(file, true);
+    }
+
+    public static BufferedImage readImageFile(String file) throws IOException {
+        return ImageIO.read(new File(file));
     }
 }
