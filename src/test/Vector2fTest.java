030211cf5151d2fa99e5096e42b7a45a6827c195
MachineMitch21 <mitchschutt@gmail.com>
2019-10-15 15:10:44 -0500

    Vector2fTest created for Vector2f tests

---
+++ src/test/Vector2fTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.junit.Test;
+
+public class Vector2fTest {
+
+    @Test
+    public void testEquals() {
+        
+    }
+
+    @Test
+    public void testAdd() {
+
+    }
+}
